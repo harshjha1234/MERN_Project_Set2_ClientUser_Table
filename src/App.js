643831f7952af791a3d42ad51e04ca11744ec5d4
harshjha1234 <74855047+harshjha1234@users.noreply.github.com>
2023-06-19 21:58:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Table from './Table';
+import "./index.css";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>User Table</h1>
+      <a href="https://github.com/harshjha1234">GitHub</a>
+      <Table />
     </div>
   );
-}
+};
 
 export default App;
